51f6696dc958a88b65cae9d58bd8d7fb341171e3
hekai <kai.he@ubtrobot.com>
2018-12-11 18:49:33 +0800

    add k-diff

--- knn.py
+++ knn.py
@@ -8,16 +8,24 @@ def createDataSet():
 
 def classify0(inX, dataSet, labels, k):
     dataSetSize = dataSet.shape[0]
-    diffMat = np.tile(inX,(dataSetSize,1)) - dataSet
-    print(diffMat)
+    #说明tile里参数列表中元组的第二个参数是控制a重复次数的
+    ######说明参数列表的元组第一个是控制行数的 
+    diffMat = np.tile(inX,(dataSetSize,1)) - dataSet  #计算差值
+    #求差值的平方
     sqDiffMat = diffMat**2
+    #每行求和
     sqDistances = sqDiffMat.sum(axis=1)
+    #开方根
     distances = sqDistances**0.5
+    print(distances)
+    #排序
     sortedDisIndicies = distances.argsort()
+    print(sortedDisIndicies)
     classCount = {}
     for i in range(k):
         voteIlabel = labels[sortedDisIndicies[i]]
         classCount[voteIlabel] = classCount.get(voteIlabel , 0) + 1
     sortedClassCount = sorted(classCount.items(),
         key = operator.itemgetter(1),reverse = True)
+    print(sortedClassCount)
     return sortedClassCount[0][0]
